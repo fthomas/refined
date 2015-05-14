9efaa90ba0f65ee2dee132d1e42aad52f3c6b9b4
Frank S. Thomas <frank@timepit.eu>
2015-05-14 22:47:27 +0200

    Test show for Forall

--- src/test/scala/eu/timepit/refined/CollectionSpec.scala
+++ src/test/scala/eu/timepit/refined/CollectionSpec.scala
@@ -7,11 +7,16 @@ import org.scalacheck.Properties
 import shapeless.nat._
 
 class CollectionSpec extends Properties("collection") {
-  property("Exists[Equal[_]]") = forAll { (l: List[Int]) =>
-    Predicate[Exists[Equal[_1]], List[Int]].isValid(l) == l.contains(1)
+  property("Exists[Equal[_]].isValid") = forAll { (l: List[Int]) =>
+    Predicate[Exists[Equal[_1]], List[Int]].isValid(l) ?= l.contains(1)
   }
 
-  property("Size[Greater[_]]") = forAll { (l: List[Int]) =>
-    Predicate[Size[Greater[_5]], List[Int]].isValid(l) == (l.size > 5)
+  property("Exists[Equal[_]].show") = secure {
+    Predicate[Exists[Equal[_1]], List[Int]].show(List(1, 2, 3)) ?=
+      "!(!(1 == 1) && !(2 == 1) && !(3 == 1))"
+  }
+
+  property("Size[Greater[_]].isValid") = forAll { (l: List[Int]) =>
+    Predicate[Size[Greater[_5]], List[Int]].isValid(l) ?= (l.size > 5)
   }
 }

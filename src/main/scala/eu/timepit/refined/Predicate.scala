e2e499531a57abe70187162d85eca9af1d6acc12
Frank S. Thomas <frank@timepit.eu>
2015-05-17 11:26:48 +0200

    Document Predicate

--- src/main/scala/eu/timepit/refined/Predicate.scala
+++ src/main/scala/eu/timepit/refined/Predicate.scala
@@ -6,15 +6,20 @@ package eu.timepit.refined
  * type class for `P`.
  */
 trait Predicate[P, T] {
-  /** Checks if `t` is valid according to the predicate `P`. */
+  /** Checks if `t` satisfies the predicate `P`. */
   def isValid(t: T): Boolean
 
   /** Returns a string representation of this [[Predicate]] using `t`. */
   def show(t: T): String
 
+  /**
+   * Returns `None` if `t` satisfies the predicate `P`, or an error message
+   * contained in `Some` otherwise.
+   */
   def validated(t: T): Option[String] =
     if (isValid(t)) None else Some(s"Predicate failed: ${show(t)}.")
 
+  /** Checks if `t` does not satisfy the predicate `P`. */
   def notValid(t: T): Boolean =
     !isValid(t)
 }

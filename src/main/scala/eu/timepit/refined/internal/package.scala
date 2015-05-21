e4bc63a53ee913b24882bb1368e8822765d00f8c
Frank S. Thomas <frank@timepit.eu>
2015-05-21 23:05:33 +0200

    Inline the shapeless.tag.apply call

--- src/main/scala/eu/timepit/refined/internal/package.scala
+++ src/main/scala/eu/timepit/refined/internal/package.scala
@@ -13,7 +13,11 @@ package object internal {
         val predicate: Predicate[P, T] = c.eval(c.Expr(c.untypecheck(p.tree)))
 
         predicate.validated(value.asInstanceOf[T]) match {
-          case None => c.Expr(q"shapeless.tag[${weakTypeOf[P]}]($t)")
+          case None =>
+            val pTpe = weakTypeOf[P]
+            val tTpe = weakTypeOf[T]
+            c.Expr(q"$t.asInstanceOf[$tTpe @@ $pTpe]")
+
           case Some(msg) => c.abort(c.enclosingPosition, msg)
         }
 

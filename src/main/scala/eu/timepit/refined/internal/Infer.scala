e0d3720c4931139f8bee6f82b208ead4b6556eff
Frank S. Thomas <frank@timepit.eu>
2015-06-06 21:45:21 +0200

    Fix bug in infer macro

--- src/main/scala/eu/timepit/refined/internal/Infer.scala
+++ src/main/scala/eu/timepit/refined/internal/Infer.scala
@@ -1,21 +1,21 @@
 package eu.timepit.refined
 package internal
 
+import eu.timepit.refined.InferenceRuleAlias.==>
 import shapeless.tag.@@
 
 import scala.reflect.macros.blackbox
 
 object Infer {
-  def macroImpl[T: c.WeakTypeTag, A: c.WeakTypeTag, B: c.WeakTypeTag](c: blackbox.Context)(t: c.Expr[T @@ A])(i: c.Expr[InferenceRule[A, B]]): c.Expr[T @@ B] = {
+  def macroImpl[T: c.WeakTypeTag, A: c.WeakTypeTag, B: c.WeakTypeTag](c: blackbox.Context)(t: c.Expr[T @@ A])(i: c.Expr[A ==> B]): c.Expr[T @@ B] = {
     import c.universe._
 
-    val tTpe = weakTypeOf[T]
-    val aTpe = weakTypeOf[A]
-    val bTpe = weakTypeOf[B]
-
     if (MacroUtils.eval(c)(i).isValid)
-      c.Expr(q"$t.asInstanceOf[$tTpe @@ $bTpe]")
-    else
+      c.Expr(q"$t.asInstanceOf[${weakTypeOf[T @@ B]}]")
+    else {
+      val aTpe = weakTypeOf[A]
+      val bTpe = weakTypeOf[B]
       c.abort(c.enclosingPosition, s"invalid inference: $aTpe ==> $bTpe")
+    }
   }
 }

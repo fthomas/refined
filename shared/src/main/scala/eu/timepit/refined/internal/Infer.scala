24be90289e234dce7e965e8c88ebbbf89e473681
Frank S. Thomas <frank@timepit.eu>
2015-06-27 21:04:39 +0200

    Minor style changes

--- shared/src/main/scala/eu/timepit/refined/internal/Infer.scala
+++ shared/src/main/scala/eu/timepit/refined/internal/Infer.scala
@@ -10,12 +10,12 @@ object Infer {
   def macroImpl[T: c.WeakTypeTag, A: c.WeakTypeTag, B: c.WeakTypeTag](c: blackbox.Context)(t: c.Expr[T @@ A])(ir: c.Expr[A ==> B]): c.Expr[T @@ B] = {
     import c.universe._
 
-    if (MacroUtils.eval(c)(ir).isValid)
+    def inferenceRule: A ==> B = MacroUtils.eval(c)(ir)
+
+    if (inferenceRule.isValid)
       c.Expr(q"$t.asInstanceOf[${weakTypeOf[T @@ B]}]")
-    else {
-      val aTpe = weakTypeOf[A]
-      val bTpe = weakTypeOf[B]
-      c.abort(c.enclosingPosition, s"invalid inference: $aTpe ==> $bTpe")
-    }
+    else
+      c.abort(c.enclosingPosition,
+        s"invalid inference: ${weakTypeOf[A]} ==> ${weakTypeOf[B]}")
   }
 }

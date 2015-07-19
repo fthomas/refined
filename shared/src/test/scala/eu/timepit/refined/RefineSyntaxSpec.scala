e9631282774abc32753e620712e7c3f3d61b9e6b
Frank S. Thomas <frank@timepit.eu>
2015-07-19 17:13:52 +0200

    Test refineMV with a type alias

--- shared/src/test/scala/eu/timepit/refined/RefineSyntaxSpec.scala
+++ shared/src/test/scala/eu/timepit/refined/RefineSyntaxSpec.scala
@@ -1,5 +1,6 @@
 package eu.timepit.refined
 
+import eu.timepit.refined.implicits._
 import eu.timepit.refined.numeric.Positive
 import org.scalacheck.Prop._
 import org.scalacheck.Properties
@@ -34,5 +35,14 @@ class RefineSyntaxSpec extends Properties("refine syntax") {
     illTyped("testRefineMT(refineMT[Positive](-1))", "Predicate.*fail.*")
     illTyped("testRefineMT(refineMT[Positive][Int](-1))", "Predicate.*fail.*")
     true
+  }
+
+  property("refineMV with type alias") = secure {
+    type PositiveInt = Int Refined Positive
+
+    val x: PositiveInt = refineMV(5)
+    val y: PositiveInt = 5
+    illTyped("val z: PositiveInt = -5", "Predicate failed: \\(-5 > 0\\).*")
+    x == y
   }
 }

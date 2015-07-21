cc31b1317174da8f7af610865a231894712ff98d
Frank S. Thomas <frank@timepit.eu>
2015-07-21 20:15:29 +0200

    Explain the return types of the refine* functions

--- shared/src/main/scala/eu/timepit/refined/package.scala
+++ shared/src/main/scala/eu/timepit/refined/package.scala
@@ -12,6 +12,10 @@ package object refined {
    * Returns `t` wrapped in `[[Refined]][T, P]` on the right if it satisfies
    * the predicate `P`, or an error message on the left otherwise.
    *
+   * @note The return type is `[[internal.Refine]][P, Refined]`, which has an
+   * `apply` method on it, allowing `refineV` to be called like in the given
+   * example.
+   *
    * @example {{{
    * scala> import eu.timepit.refined._
    *      | import eu.timepit.refined.numeric._
@@ -23,8 +27,12 @@ package object refined {
   def refineV[P]: Refine[P, Refined] = new Refine[P, Refined]
 
   /**
-   * Returns `t` with type `T @@ P` on the right if it satisfies the predicate
-   * `P`, or an error message on the left otherwise.
+   * Returns `t` with type `T @@ P` on the right if it satisfies the
+   * predicate `P`, or an error message on the left otherwise.
+   *
+   * @note The return type is `[[internal.Refine]][P, @@]`, which has an
+   * `apply` method on it, allowing `refineT` to be called like in the given
+   * example.
    *
    * @example {{{
    * scala> import eu.timepit.refined._
@@ -41,6 +49,10 @@ package object refined {
    * Macro that returns `t` wrapped in `[[Refined]][T, P]` if it satisfies
    * the predicate `P`, or fails to compile.
    *
+   * @note The return type is `[[internal.RefineM]][P, Refined]`, which has
+   * an `apply` method on it, allowing `refineMV` to be called like in the
+   * given example.
+   *
    * @example {{{
    * scala> import eu.timepit.refined._
    *      | import eu.timepit.refined.numeric._
@@ -52,8 +64,12 @@ package object refined {
   def refineMV[P]: RefineM[P, Refined] = new RefineM[P, Refined]
 
   /**
-   * Macro that returns `t` with type `T @@ P` if it satisfies the predicate `P`,
-   * or fails to compile.
+   * Macro that returns `t` with type `T @@ P` if it satisfies the predicate
+   * `P`, or fails to compile.
+   *
+   * @note The return type is `[[internal.RefineM]][P, @@]`, which has an
+   * `apply` method on it, allowing `refineMT` to be called like in the given
+   * example.
    *
    * @example {{{
    * scala> import eu.timepit.refined._
@@ -65,7 +81,6 @@ package object refined {
    * }}}
    */
   def refineMT[P]: RefineM[P, @@] = new RefineM[P, @@]
-
 
   @deprecated("refine is deprecated in favor of refineT", "0.2.0")
   def refine[P]: Refine[P, @@] = new Refine[P, @@]

8f1e2e9f04e07d38dd077e31a7fbca1044a69750
Frank S. Thomas <f.thomas@gmx.de>
2015-07-21 17:02:10 +0200

    Add examples to the refine functions

--- shared/src/main/scala/eu/timepit/refined/package.scala
+++ shared/src/main/scala/eu/timepit/refined/package.scala
@@ -11,24 +11,44 @@ package object refined {
   /**
    * Returns `t` wrapped in `[[Refined]][T, P]` on the right if it satisfies
    * the predicate `P`, or an error message on the left otherwise.
+   *
+   * @example {{{
+   * scala> refineV[Positive](10)
+   * res1: Either[String, Refined[Int, Positive]] = Right(Refined(10))
+   * }}}
    */
   def refineV[P]: Refine[P, Refined] = new Refine[P, Refined]
 
   /**
    * Returns `t` with type `T @@ P` on the right if it satisfies the predicate
    * `P`, or an error message on the left otherwise.
+   *
+   * @example {{{
+   * scala> refineT[Positive](10)
+   * res1: Either[String, Int @@ Positive] = Right(10)
+   * }}}
    */
   def refineT[P]: Refine[P, @@] = new Refine[P, @@]
 
   /**
    * Macro that returns `t` wrapped in `[[Refined]][T, P]` if it satisfies
    * the predicate `P`, or fails to compile.
+   *
+   * @example {{{
+   * scala> refineMV[Positive](10)
+   * res1: Refined[Int, Positive] = Refined(10)
+   * }}}
    */
   def refineMV[P]: RefineM[P, Refined] = new RefineM[P, Refined]
 
   /**
    * Macro that returns `t` with type `T @@ P` if it satisfies the predicate `P`,
    * or fails to compile.
+   *
+   * @example {{{
+   * scala> refineMT[Positive](10)
+   * res1: Int @@ Positive = 10
+   * }}}
    */
   def refineMT[P]: RefineM[P, @@] = new RefineM[P, @@]
 

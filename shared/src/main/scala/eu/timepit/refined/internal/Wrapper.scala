7a225958371f87f7922fde1ac94909fb2f7a32c4
Frank S. Thomas <frank@timepit.eu>
2015-08-01 10:47:20 +0200

    Add Wrapper.unwrap and formulate a Wrapper law

--- shared/src/main/scala/eu/timepit/refined/internal/Wrapper.scala
+++ shared/src/main/scala/eu/timepit/refined/internal/Wrapper.scala
@@ -13,6 +13,8 @@ trait Wrapper[F[_, _]] {
 
   def wrap[T, P](t: T): F[T, P]
 
+  def unwrap[T, P](tp: F[T, P]): T
+
   def wrapM[T: c.WeakTypeTag, P: c.WeakTypeTag](c: blackbox.Context)(t: c.Expr[T]): c.Expr[F[T, P]]
 
   def rewrapM[T: c.WeakTypeTag, A: c.WeakTypeTag, B: c.WeakTypeTag](c: blackbox.Context)(ta: c.Expr[F[T, A]]): c.Expr[F[T, B]]
@@ -24,8 +26,9 @@ object Wrapper {
 
   implicit def refinedWrapper: Wrapper[Refined] =
     new Wrapper[Refined] {
-      override def wrap[T, P](t: T): Refined[T, P] =
-        Refined(t)
+      override def wrap[T, P](t: T): Refined[T, P] = Refined(t)
+
+      override def unwrap[T, P](tp: Refined[T, P]): T = tp.get
 
       override def wrapM[T: c.WeakTypeTag, P: c.WeakTypeTag](c: blackbox.Context)(t: c.Expr[T]): c.Expr[Refined[T, P]] =
         c.universe.reify(Refined[T, P](t.splice))
@@ -36,8 +39,9 @@ object Wrapper {
 
   implicit def tagWrapper: Wrapper[@@] =
     new Wrapper[@@] {
-      override def wrap[T, P](t: T): T @@ P =
-        t.asInstanceOf[T @@ P]
+      override def wrap[T, P](t: T): T @@ P = t.asInstanceOf[T @@ P]
+
+      override def unwrap[T, P](tp: T @@ P): T = tp
 
       override def wrapM[T: c.WeakTypeTag, P: c.WeakTypeTag](c: blackbox.Context)(t: c.Expr[T]): c.Expr[T @@ P] =
         c.universe.reify(t.splice.asInstanceOf[T @@ P])

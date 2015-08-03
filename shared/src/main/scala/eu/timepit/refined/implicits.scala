b77fd997dadf492455e5334b30f7cc33596ccd34
Frank S. Thomas <frank@timepit.eu>
2015-08-03 21:32:45 +0200

    Document autoRefine[VT]

--- shared/src/main/scala/eu/timepit/refined/implicits.scala
+++ shared/src/main/scala/eu/timepit/refined/implicits.scala
@@ -16,11 +16,25 @@ object implicits {
     ir: A ==> B, w: Wrapper[F]
   ): F[T, B] = macro InferM.macroImpl[T, A, B, F]
 
+  /**
+   * Implicitly wraps (at compile-time) a value of type `T` in
+   * `[[Refined]][T, P]` if `t` satisfies the predicate `P`. If it does not
+   * satisfy the predicate, compilation fails.
+   *
+   * This is an implicit version of `[[refineMV]]`.
+   */
   implicit def autoRefineV[T, P](t: T)(
     implicit
     p: Predicate[P, T], w: Wrapper[Refined]
   ): Refined[T, P] = macro RefineMAux.macroImpl[P, T, Refined]
 
+  /**
+   * Implicitly tags (at compile-time) a value of type `T` with `P` if `t`
+   * satisfies the predicate `P`. If it does not satisfy the predicate,
+   * compilation fails.
+   *
+   * This is an implicit version of `[[refineMT]]`.
+   */
   implicit def autoRefineT[T, P](t: T)(
     implicit
     p: Predicate[P, T], w: Wrapper[@@]

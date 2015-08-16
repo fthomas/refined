988b8a5d232f174e11846a8b20e5e8eb44a105ec
Frank S. Thomas <frank@timepit.eu>
2015-08-16 20:15:49 +0200

    Merge RefineSpec with RefTypeSpec

--- shared/src/test/scala/eu/timepit/refined/RefTypeSpec.scala
+++ shared/src/test/scala/eu/timepit/refined/RefTypeSpec.scala
@@ -1,9 +1,13 @@
 package eu.timepit.refined
 
 import eu.timepit.refined.RefType.ops._
-import eu.timepit.refined.numeric.Positive
+import eu.timepit.refined.char.{Digit, LowerCase}
+import eu.timepit.refined.collection.Forall
+import eu.timepit.refined.numeric._
+import eu.timepit.refined.string.MatchesRegex
 import org.scalacheck.Prop._
 import org.scalacheck.Properties
+import shapeless.nat._
 import shapeless.tag.@@
 
 abstract class RefTypeProperties[F[_, _]](name: String)(implicit rt: RefType[F]) extends Properties(s"RefType[$name]") {
@@ -12,11 +16,37 @@ abstract class RefTypeProperties[F[_, _]](name: String)(implicit rt: RefType[F])
     rt.unsafeWrap(s).unwrap == s
   }
 
-  property("mapRefine.success") = secure {
+  property("RefineAux instance") = secure {
+    val aux = rt.refine[Digit]
+    aux('0').isRight
+  }
+
+  property("refine success with Less") = secure {
+    rt.refine[Less[W.`100`.T]](-100).isRight
+  }
+
+  property("refine success with Greater") = secure {
+    rt.refine[Greater[_5]](6).isRight
+  }
+
+  property("refine failure with Interval") = secure {
+    rt.refine[Interval[W.`-0.5`.T, W.`0.5`.T]](0.6).isLeft
+  }
+
+  property("refine failure with Forall") = secure {
+    rt.refine[Forall[LowerCase]]("Hallo").isLeft
+  }
+
+  property("refine success with MatchesRegex") = secure {
+    type DigitsOnly = MatchesRegex[W.`"[0-9]+"`.T]
+    rt.refine[DigitsOnly]("123").isRight
+  }
+
+  property("mapRefine success with Positive") = secure {
     rt.refine[Positive](5).right.flatMap(_.mapRefine(_.toDouble)).isRight
   }
 
-  property("mapRefine.failure") = secure {
+  property("mapRefine failure with Positive") = secure {
     rt.refine[Positive](5).right.flatMap(_.mapRefine(_ - 10)).isLeft
   }
 }

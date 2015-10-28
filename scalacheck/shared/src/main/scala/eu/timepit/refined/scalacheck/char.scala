e5ed891ac82cd7635614a3b00ffbbd9ef1154517
Frank S. Thomas <frank@timepit.eu>
2015-10-28 18:23:05 +0100

    Generate the list of whitespace chars

--- scalacheck/shared/src/main/scala/eu/timepit/refined/scalacheck/char.scala
+++ scalacheck/shared/src/main/scala/eu/timepit/refined/scalacheck/char.scala
@@ -20,10 +20,8 @@ object char {
     arbitraryRefType(Gen.alphaUpperChar)
 
   implicit def whitespaceArbitrary[F[_, _]: RefType]: Arbitrary[F[Char, Whitespace]] =
-    arbitraryRefType(Gen.oneOf(
-      // https://en.wikipedia.org/wiki/Whitespace_character
-      '\u0009', '\u000A', '\u000B', '\u000C', '\u000D', '\u0020',
-      '\u2000', '\u2001', '\u2002', '\u2003', '\u2004', '\u2005', '\u2006', '\u2008', '\u2009', '\u200A',
-      '\u2028', '\u2029', '\u205F'
-    ))
+    arbitraryRefType(Gen.oneOf(whitespaceChars))
+
+  private val whitespaceChars: Seq[Char] =
+    (Char.MinValue to Char.MaxValue).filter(_.isWhitespace)
 }

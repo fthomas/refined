3266667e6a886d9b7f6721866e8156a239953264
Frank S. Thomas <frank@timepit.eu>
2015-11-02 13:44:53 +0100

    Better tests for Eval

--- core/jvm/src/test/scala/eu/timepit/refined/GenericValidateSpecJvm.scala
+++ core/jvm/src/test/scala/eu/timepit/refined/GenericValidateSpecJvm.scala
@@ -1,18 +1,19 @@
 package eu.timepit.refined
 
-import eu.timepit.refined.TestUtils._
+import eu.timepit.refined.api.Validate
 import eu.timepit.refined.generic.Eval
 import org.scalacheck.Prop._
 import org.scalacheck.Properties
 
 class GenericValidateSpecJvm extends Properties("GenericValidate") {
 
-  property("Eval.isValid") = secure {
-    isValid[Eval[W.`"(x: Int) => x % 2 == 0"`.T]](6) &&
-      notValid[Eval[W.`"(x: Int) => x % 2 == 0"`.T]](7)
+  val even = Validate[Int, Eval[W.`"(x: Int) => x % 2 == 0"`.T]]
+
+  property("Eval.isValid") = forAll { (i: Int) =>
+    even.isValid(i) ?= (i % 2 == 0)
   }
 
   property("Eval.showExpr") = secure {
-    showExpr[Eval[W.`"(x: Int) => x % 2 == 0"`.T]](0) ?= "(x: Int) => x % 2 == 0"
+    even.showExpr(0) ?= "(x: Int) => x % 2 == 0"
   }
 }

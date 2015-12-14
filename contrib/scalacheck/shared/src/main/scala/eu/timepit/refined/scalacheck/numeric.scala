ae0379278b146afd9c3d41bff67d82aeab43b160
Jean-Rémi Desjardins <jeanremi.desjardins@gmail.com>
2015-12-13 23:25:45 -0500

    Revamp instances of Arbitrary for Numeric
    - Remove hardcoded Int
    - Add instances for shapeless.Nat

--- contrib/scalacheck/shared/src/main/scala/eu/timepit/refined/scalacheck/numeric.scala
+++ contrib/scalacheck/shared/src/main/scala/eu/timepit/refined/scalacheck/numeric.scala
@@ -3,92 +3,171 @@ package scalacheck
 
 import eu.timepit.refined.api.RefType
 import eu.timepit.refined.numeric._
+import eu.timepit.refined.boolean.Not
+import org.scalacheck.Gen.Choose
 import org.scalacheck.{ Arbitrary, Gen }
-import shapeless.Witness
+import shapeless.ops.nat.ToInt
+import shapeless.{ Nat, Witness }
 
 object numeric {
 
-  implicit def lessArbitrary[F[_, _], T, N <: T](
+  trait Bounded[T] extends {
+    def minValue: T
+    def maxValue: T
+  }
+
+  object Bounded {
+    def apply[T](min: T, max: T): Bounded[T] = new Bounded[T] {
+      val minValue = min
+      val maxValue = max
+    }
+    implicit val int: Bounded[Int] = Bounded(Int.MinValue, Int.MaxValue)
+    implicit val short: Bounded[Short] = Bounded(Short.MinValue, Short.MaxValue)
+    implicit val long: Bounded[Long] = Bounded(Long.MinValue, Long.MaxValue)
+  }
+
+  implicit def lessArbitrary[F[_, _]: RefType, T: Choose: Numeric: Bounded, N <: T](implicit wn: Witness.Aux[N]): Arbitrary[F[T, Less[N]]] =
+    lessArbitraryImpl(wn.value)
+
+  implicit def lessArbitraryNat[F[_, _]: RefType, T: Choose: Bounded, N <: Nat: ToInt](implicit num: Numeric[T]): Arbitrary[F[T, Less[N]]] =
+    lessArbitraryImpl(num.fromInt(Nat.toInt[N]))
+
+  private def lessArbitraryImpl[F[_, _]: RefType, T: Choose, N](value: T)(
     implicit
-    rt: RefType[F],
-    wn: Witness.Aux[N],
-    nt: Numeric[T],
-    c: Gen.Choose[T]
+    num: Numeric[T],
+    bound: Bounded[T]
   ): Arbitrary[F[T, Less[N]]] = {
-    val gen = Gen.chooseNum(nt.fromInt(Int.MinValue), wn.value).filter(nt.lt(_, wn.value))
+    val gen = Gen.chooseNum(bound.minValue, value).filter(!num.equiv(_, value))
     arbitraryRefType(gen)
   }
 
-  implicit def lessEqualArbitrary[F[_, _], T, N <: T](
+  implicit def lessEqualArbitrary[F[_, _]: RefType, T: Choose, N <: T](
     implicit
-    rt: RefType[F],
     wn: Witness.Aux[N],
     nt: Numeric[T],
-    c: Gen.Choose[T]
+    bound: Bounded[T]
   ): Arbitrary[F[T, LessEqual[N]]] = {
-    val gen = Gen.chooseNum(nt.fromInt(Int.MinValue), wn.value)
+    val gen = Gen.chooseNum(bound.minValue, wn.value)
     arbitraryRefType(gen)
   }
 
-  implicit def greaterArbitrary[F[_, _], T, N <: T](
+  implicit def lessEqualArbitraryNat[F[_, _]: RefType, T: Choose, N <: Nat: ToInt](
     implicit
-    rt: RefType[F],
+    num: Numeric[T],
+    bound: Bounded[T]
+  ): Arbitrary[F[T, LessEqual[N]]] = {
+    val gen = Gen.chooseNum(bound.minValue, num.fromInt(Nat.toInt[N]))
+    arbitraryRefType(gen)
+  }
+
+  implicit def greaterArbitrary[F[_, _]: RefType, T: Choose: Numeric, N <: T](
+    implicit
     wn: Witness.Aux[N],
-    nt: Numeric[T],
-    c: Gen.Choose[T]
+    bound: Bounded[T]
+  ): Arbitrary[F[T, Greater[N]]] =
+    greaterArbitraryImpl(min = wn.value, max = bound.maxValue)
+
+  implicit def greaterArbitraryNat[F[_, _]: RefType, T: Choose, N <: Nat: ToInt](
+    implicit
+    num: Numeric[T],
+    bound: Bounded[T]
+  ): Arbitrary[F[T, Greater[N]]] =
+    greaterArbitraryImpl(min = num.fromInt(Nat.toInt[N]), max = bound.maxValue)
+
+  private def greaterArbitraryImpl[F[_, _]: RefType, T: Choose, N](min: T, max: T)(
+    implicit
+    num: Numeric[T]
   ): Arbitrary[F[T, Greater[N]]] = {
-    val gen = Gen.chooseNum(wn.value, nt.fromInt(Int.MaxValue)).filter(nt.gt(_, wn.value))
+    val gen = Gen.chooseNum(min, max).filter(!num.equiv(_, min))
     arbitraryRefType(gen)
   }
 
-  implicit def greaterEqualArbitrary[F[_, _], T, N <: T](
+  def greaterArbitraryRange[F[_, _], T: Choose: Numeric, N <: Nat](min: F[T, Greater[N]], max: F[T, Greater[N]])(
+    implicit
+    rt: RefType[F]
+  ): Arbitrary[F[T, Greater[N]]] =
+    greaterArbitraryImpl(min = rt.unwrap(min), max = rt.unwrap(max))
+
+  def greaterArbitraryMax[F[_, _], T: Choose, N <: Nat: ToInt](max: F[T, Greater[N]])(
     implicit
     rt: RefType[F],
+    num: Numeric[T]
+  ): Arbitrary[F[T, Greater[N]]] =
+    greaterArbitraryImpl(min = num.fromInt(Nat.toInt[N]), max = rt.unwrap(max))
+
+  implicit def greaterEqualArbitrary[F[_, _]: RefType, T: Choose, N <: T](
+    implicit
     wn: Witness.Aux[N],
     nt: Numeric[T],
-    c: Gen.Choose[T]
+    bound: Bounded[T]
   ): Arbitrary[F[T, GreaterEqual[N]]] = {
-    val gen = Gen.chooseNum(wn.value, nt.fromInt(Int.MaxValue))
+    val gen = Gen.chooseNum(wn.value, bound.maxValue)
     arbitraryRefType(gen)
   }
 
-  implicit def intervalOpenArbitrary[F[_, _], T, L <: T, H <: T](
+  implicit def intervalOpenArbitrary[F[_, _]: RefType, T: Choose, L <: T, H <: T](
     implicit
-    rt: RefType[F],
     wl: Witness.Aux[L],
     wh: Witness.Aux[H],
-    nt: Numeric[T],
-    c: Gen.Choose[T]
-  ): Arbitrary[F[T, Interval.Open[L, H]]] =
-    arbitraryRefType(Gen.chooseNum(wl.value, wh.value).filter(t => nt.gt(t, wl.value) && nt.lt(t, wh.value)))
+    nt: Numeric[T]
+  ): Arbitrary[F[T, Interval.Open[L, H]]] = {
+    val gen = Gen.chooseNum(wl.value, wh.value).filter(t => nt.gt(t, wl.value) && nt.lt(t, wh.value))
+    arbitraryRefType(gen)
+  }
 
-  implicit def intervalOpenClosedArbitrary[F[_, _], T, L <: T, H <: T](
+  implicit def intervalOpenClosedArbitrary[F[_, _]: RefType, T: Choose, L <: T, H <: T](
     implicit
-    rt: RefType[F],
     wl: Witness.Aux[L],
     wh: Witness.Aux[H],
-    nt: Numeric[T],
-    c: Gen.Choose[T]
-  ): Arbitrary[F[T, Interval.OpenClosed[L, H]]] =
-    arbitraryRefType(Gen.chooseNum(wl.value, wh.value).filter(t => nt.gt(t, wl.value)))
+    nt: Numeric[T]
+  ): Arbitrary[F[T, Interval.OpenClosed[L, H]]] = {
+    val gen = Gen.chooseNum(wl.value, wh.value).filter(t => nt.gt(t, wl.value))
+    arbitraryRefType(gen)
+  }
 
-  implicit def intervalClosedOpenArbitrary[F[_, _], T, L <: T, H <: T](
+  implicit def intervalClosedOpenArbitrary[F[_, _]: RefType, T: Choose, L <: T, H <: T](
     implicit
-    rt: RefType[F],
     wl: Witness.Aux[L],
     wh: Witness.Aux[H],
-    nt: Numeric[T],
-    c: Gen.Choose[T]
-  ): Arbitrary[F[T, Interval.ClosedOpen[L, H]]] =
-    arbitraryRefType(Gen.chooseNum(wl.value, wh.value).filter(t => nt.lt(t, wh.value)))
+    nt: Numeric[T]
+  ): Arbitrary[F[T, Interval.ClosedOpen[L, H]]] = {
+    val gen = Gen.chooseNum(wl.value, wh.value).filter(t => nt.lt(t, wh.value))
+    arbitraryRefType(gen)
+  }
 
-  implicit def intervalClosedArbitrary[F[_, _], T, L <: T, H <: T](
+  implicit def intervalClosedArbitrary[F[_, _]: RefType, T: Choose, L <: T, H <: T](
     implicit
-    rt: RefType[F],
     wl: Witness.Aux[L],
     wh: Witness.Aux[H],
-    nt: Numeric[T],
-    c: Gen.Choose[T]
-  ): Arbitrary[F[T, Interval.Closed[L, H]]] =
-    arbitraryRefType(Gen.chooseNum(wl.value, wh.value))
+    nt: Numeric[T]
+  ): Arbitrary[F[T, Interval.Closed[L, H]]] = {
+    val gen = Gen.chooseNum(wl.value, wh.value)
+    arbitraryRefType(gen)
+  }
+
+  implicit def notLessArbitraryNat[F[_, _]: RefType, T: Choose, N <: Nat: ToInt](
+    implicit
+    num: Numeric[T],
+    bound: Bounded[T]
+  ): Arbitrary[F[T, Not[Less[N]]]] = {
+    val gen = Gen.chooseNum(num.fromInt(Nat.toInt[N]), bound.maxValue)
+    arbitraryRefType(gen)
+  }
+
+  def notLessArbitraryRange[F[_, _], T: Choose: Numeric, N <: Nat: ToInt](min: F[T, Not[Less[N]]], max: F[T, Not[Less[N]]])(
+    implicit
+    rt: RefType[F]
+  ): Arbitrary[F[T, Not[Less[N]]]] = {
+    val gen = Gen.chooseNum(rt.unwrap(min), rt.unwrap(max))
+    arbitraryRefType(gen)
+  }
+
+  def notLessArbitraryMax[F[_, _], T: Choose, N <: Nat: ToInt](max: F[T, Not[Less[N]]])(
+    implicit
+    rt: RefType[F],
+    num: Numeric[T]
+  ): Arbitrary[F[T, Not[Less[N]]]] = {
+    val gen = Gen.chooseNum(num.fromInt(Nat.toInt[N]), rt.unwrap(max))
+    arbitraryRefType(gen)
+  }
 }
--- contrib/scalacheck/shared/src/test/scala/eu/timepit/refined/scalacheck/NumericArbitrarySpec.scala
+++ contrib/scalacheck/shared/src/test/scala/eu/timepit/refined/scalacheck/NumericArbitrarySpec.scala
@@ -1,13 +1,16 @@
 package eu.timepit.refined
 package scalacheck
 
-import eu.timepit.refined.api.Refined
+import eu.timepit.refined.api.{ RefType, Validate, Refined }
 import eu.timepit.refined.auto._
+import eu.timepit.refined.boolean.Not
 import eu.timepit.refined.numeric._
 import eu.timepit.refined.scalacheck.numeric._
 import eu.timepit.refined.util.time.Minute
 import org.scalacheck.Prop._
 import org.scalacheck.Properties
+
+import shapeless.Nat
 
 class NumericArbitrarySpec extends Properties("NumericArbitrary") {
 
@@ -17,11 +20,17 @@ class NumericArbitrarySpec extends Properties("NumericArbitrary") {
   property("Less.negative") =
     checkArbitraryRefType[Refined, Int, Less[W.`-100`.T]]
 
+  property("Less.natural") =
+    checkArbitraryRefType[Refined, Long, Less[Nat._10]]
+
   property("LessEqual.positive") =
     checkArbitraryRefType[Refined, Int, LessEqual[W.`42`.T]]
 
   property("LessEqual.negative") =
     checkArbitraryRefType[Refined, Int, LessEqual[W.`-42`.T]]
+
+  property("LessEqual.natural") =
+    checkArbitraryRefType[Refined, Long, LessEqual[Nat._10]]
 
   property("Greater.positive") =
     checkArbitraryRefType[Refined, Int, Greater[W.`100`.T]]
@@ -29,11 +38,26 @@ class NumericArbitrarySpec extends Properties("NumericArbitrary") {
   property("Greater.negative") =
     checkArbitraryRefType[Refined, Int, Greater[W.`-100`.T]]
 
+  property("Greater.natural") =
+    checkArbitraryRefType[Refined, Long, Greater[Nat._10]]
+
+  property("Greater range") =
+    forAll { (min: Refined[Long, Greater[Nat._10]], max: Refined[Long, Greater[Nat._10]]) =>
+      checkArbitraryRefType[Refined, Long, Greater[Nat._10]](
+        greaterArbitraryRange[Refined, Long, Nat._10](min, max),
+        implicitly[RefType[Refined]],
+        Validate.fromPredicate((t: Long) => true, (t: Long) => t.toString, Greater[Nat._10](Nat(10)))
+      )
+    }
+
   property("GreaterEqual.positive") =
     checkArbitraryRefType[Refined, Int, GreaterEqual[W.`123456`.T]]
 
   property("GreaterEqual.negative") =
     checkArbitraryRefType[Refined, Int, GreaterEqual[W.`-123456`.T]]
+
+  property("NotLess.natural") =
+    checkArbitraryRefType[Refined, Long, Not[Less[Nat._10]]]
 
   property("Interval.Open") =
     checkArbitraryRefType[Refined, Int, Interval.Open[W.`-23`.T, W.`42`.T]]

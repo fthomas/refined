f6cc5742fe555e6ab4ea94e95bf984c46eb703a8
Frank S. Thomas <frank@timepit.eu>
2016-03-18 17:19:58 +0100

    Fix return type of ApplyRefPartiallyApplied.apply
    This changes the return type of `ApplyRefPartiallyApplied.apply`
    such that `res0` in the following example contains `Natural`:
    
    ```scala
    scala> type Natural = Long Refined NonNegative
    defined type alias Natural
    
    scala> RefType.applyRef[Natural](4L)
    res0: Either[String,Natural] = Right(Refined(4))
    ```
    
    Without this change, we would get:
    ```scala
    scala> RefType.applyRef[Natural](4L)
    res0: Either[String,Long Refined NonNegative] = Right(Refined(4))
    ```

--- core/shared/src/main/scala/eu/timepit/refined/internal/ApplyRefPartiallyApplied.scala
+++ core/shared/src/main/scala/eu/timepit/refined/internal/ApplyRefPartiallyApplied.scala
@@ -14,7 +14,7 @@ final class ApplyRefPartiallyApplied[FTP] {
 
   def apply[F[_, _], T, P](t: T)(
     implicit
-    ev: FTP =:= F[T, P], rt: RefType[F], v: Validate[T, P]
-  ): Either[String, F[T, P]] =
-    rt.refine[P](t)
+    ev: F[T, P] =:= FTP, rt: RefType[F], v: Validate[T, P]
+  ): Either[String, FTP] =
+    rt.refine[P](t).right.map(ev)
 }

e1d4164afddc6a92c424118668ccee189e7ea005
Frank S. Thomas <frank@timepit.eu>
2016-03-19 16:55:31 +0100

    Add RefType.applyRefM
    Add `RefType.applyRefM`, the the macro variant of `RefType.applyRef`.
    `applyRefM` is useful when working with type aliases for refined
    types and without any implicits, for example:
    
    ```scala
    scala> type Natural = Long @@ NonNegative
    defined type alias Natural
    
    scala> RefType.applyRefM[Natural](42L)
    res0: Natural = 42
    ```
    
    Closes #112.

---
+++ core/shared/src/main/scala/eu/timepit/refined/internal/ApplyRefMPartiallyApplied.scala
@@ -0,0 +1,19 @@
+package eu.timepit.refined
+package internal
+
+import eu.timepit.refined.api.{ RefType, Validate }
+
+/**
+ * Helper class that allows the types `F`, `T`, and `P` to be inferred
+ * from calls like `[[api.RefType.applyRefM]][F[T, P]](t)`.
+ *
+ * See [[http://tpolecat.github.io/2015/07/30/infer.html]] for a detailed
+ * explanation of this trick.
+ */
+final class ApplyRefMPartiallyApplied[FTP] {
+
+  def apply[F[_, _], T, P](t: T)(
+    implicit
+    ev: F[T, P] =:= FTP, rt: RefType[F], v: Validate[T, P]
+  ): FTP = macro macros.RefineMacro.implApplyRef[FTP, F, T, P]
+}

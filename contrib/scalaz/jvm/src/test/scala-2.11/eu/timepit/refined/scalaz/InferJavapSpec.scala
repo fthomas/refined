265896349426de4492a5fb53efc4161b40caf88a
Frank S. Thomas <frank@timepit.eu>
2016-04-07 22:01:10 +0200

    DRY tests

--- contrib/scalaz/jvm/src/test/scala-2.11/eu/timepit/refined/scalaz/InferJavapSpec.scala
+++ contrib/scalaz/jvm/src/test/scala-2.11/eu/timepit/refined/scalaz/InferJavapSpec.scala
@@ -12,40 +12,28 @@ import org.scalacheck.Properties
 
 class InferAnyValTest {
 
-  def Refined1: Refined[Int, Greater[W.`1`.T]] = 10
+  val r = new RefineAnyValTest
 
-  def Refined0: Refined[Int, Greater[W.`0`.T]] = Refined1
+  def Refined: Refined[Int, Greater[W.`0`.T]] = r.Refined
 
-  def shapeless_tag1: shapeless.tag.@@[Int, Greater[W.`1`.T]] = 10
+  def shapeless_tag: shapeless.tag.@@[Int, Greater[W.`0`.T]] = r.shapeless_tag
 
-  def shapeless_tag0: shapeless.tag.@@[Int, Greater[W.`0`.T]] = shapeless_tag1
+  def scalaz_tag: scalaz.@@[Int, Greater[W.`0`.T]] = r.scalaz_tag
 
-  def scalaz_tag1: scalaz.@@[Int, Greater[W.`1`.T]] = 10
-
-  def scalaz_tag0: scalaz.@@[Int, Greater[W.`0`.T]] = scalaz_tag1
-
-  def unrefined1: Int = 10
-
-  def unrefined0: Int = unrefined1
+  def unrefined: Int = r.unrefined
 }
 
 class InferAnyRefTest {
 
-  def Refined1: Refined[String, StartsWith[W.`"abc"`.T]] = "abc"
+  val r = new RefineAnyRefTest
 
-  def Refined0: Refined[String, StartsWith[W.`"ab"`.T]] = Refined1
+  def Refined: Refined[String, StartsWith[W.`"ab"`.T]] = r.Refined
 
-  def shapeless_tag1: shapeless.tag.@@[String, StartsWith[W.`"abc"`.T]] = "abc"
+  def shapeless_tag: shapeless.tag.@@[String, StartsWith[W.`"ab"`.T]] = r.shapeless_tag
 
-  def shapeless_tag0: shapeless.tag.@@[String, StartsWith[W.`"ab"`.T]] = shapeless_tag1
+  def scalaz_tag: scalaz.@@[String, StartsWith[W.`"ab"`.T]] = r.scalaz_tag
 
-  def scalaz_tag1: scalaz.@@[String, StartsWith[W.`"abc"`.T]] = "abc"
-
-  def scalaz_tag0: scalaz.@@[String, StartsWith[W.`"ab"`.T]] = scalaz_tag1
-
-  def unrefined1: String = "abc"
-
-  def unrefined0: String = unrefined1
+  def unrefined: String = r.unrefined
 }
 
 class InferJavapSpec extends Properties("InferJavapTest") {
@@ -55,59 +43,50 @@ class InferJavapSpec extends Properties("InferJavapTest") {
     val expected =
       """Compiled from "InferJavapSpec.scala"
         |public class eu.timepit.refined.scalaz.InferAnyValTest {
-        |  public int Refined1();
-        |    Code:
-        |       0: getstatic     #16                 // Field eu/timepit/refined/api/Refined$.MODULE$:Leu/timepit/refined/api/Refined$;
-        |       3: bipush        10
-        |       5: invokestatic  #22                 // Method scala/runtime/BoxesRunTime.boxToInteger:(I)Ljava/lang/Integer;
-        |       8: invokevirtual #26                 // Method eu/timepit/refined/api/Refined$.unsafeApply:(Ljava/lang/Object;)Ljava/lang/Object;
-        |      11: checkcast     #28                 // class java/lang/Integer
-        |      14: areturn
-        |  public int Refined0();
-        |    Code:
-        |       0: getstatic     #16                 // Field eu/timepit/refined/api/Refined$.MODULE$:Leu/timepit/refined/api/Refined$;
-        |       3: aload_0
-        |       4: invokevirtual #33                 // Method Refined1:()Ljava/lang/Integer;
-        |       7: invokevirtual #26                 // Method eu/timepit/refined/api/Refined$.unsafeApply:(Ljava/lang/Object;)Ljava/lang/Object;
-        |      10: checkcast     #28                 // class java/lang/Integer
-        |      13: areturn
-        |  public java.lang.Object shapeless_tag1();
-        |    Code:
-        |       0: bipush        10
-        |       2: invokestatic  #22                 // Method scala/runtime/BoxesRunTime.boxToInteger:(I)Ljava/lang/Integer;
-        |       5: invokestatic  #39                 // Method scala/runtime/BoxesRunTime.unboxToInt:(Ljava/lang/Object;)I
-        |       8: ireturn
-        |  public java.lang.Object shapeless_tag0();
+        |  public eu.timepit.refined.scalaz.RefineAnyValTest r();
         |    Code:
         |       0: aload_0
-        |       1: invokevirtual #42                 // Method shapeless_tag1:()I
-        |       4: invokestatic  #22                 // Method scala/runtime/BoxesRunTime.boxToInteger:(I)Ljava/lang/Integer;
-        |       7: invokestatic  #39                 // Method scala/runtime/BoxesRunTime.unboxToInt:(Ljava/lang/Object;)I
-        |      10: ireturn
-        |  public java.lang.Object scalaz_tag1();
-        |    Code:
-        |       0: bipush        10
-        |       2: invokestatic  #22                 // Method scala/runtime/BoxesRunTime.boxToInteger:(I)Ljava/lang/Integer;
-        |       5: areturn
-        |  public java.lang.Object scalaz_tag0();
-        |    Code:
-        |       0: aload_0
-        |       1: invokevirtual #47                 // Method scalaz_tag1:()Ljava/lang/Object;
+        |       1: getfield      #13                 // Field r:Leu/timepit/refined/scalaz/RefineAnyValTest;
         |       4: areturn
-        |  public int unrefined1();
+        |  public int Refined();
         |    Code:
-        |       0: bipush        10
-        |       2: ireturn
-        |  public int unrefined0();
+        |       0: getstatic     #23                 // Field eu/timepit/refined/api/Refined$.MODULE$:Leu/timepit/refined/api/Refined$;
+        |       3: aload_0
+        |       4: invokevirtual #25                 // Method r:()Leu/timepit/refined/scalaz/RefineAnyValTest;
+        |       7: invokevirtual #29                 // Method eu/timepit/refined/scalaz/RefineAnyValTest.Refined:()Ljava/lang/Integer;
+        |      10: invokevirtual #33                 // Method eu/timepit/refined/api/Refined$.unsafeApply:(Ljava/lang/Object;)Ljava/lang/Object;
+        |      13: checkcast     #35                 // class java/lang/Integer
+        |      16: areturn
+        |  public java.lang.Object shapeless_tag();
         |    Code:
         |       0: aload_0
-        |       1: invokevirtual #51                 // Method unrefined1:()I
-        |       4: ireturn
+        |       1: invokevirtual #25                 // Method r:()Leu/timepit/refined/scalaz/RefineAnyValTest;
+        |       4: invokevirtual #39                 // Method eu/timepit/refined/scalaz/RefineAnyValTest.shapeless_tag:()I
+        |       7: invokestatic  #45                 // Method scala/runtime/BoxesRunTime.boxToInteger:(I)Ljava/lang/Integer;
+        |      10: invokestatic  #49                 // Method scala/runtime/BoxesRunTime.unboxToInt:(Ljava/lang/Object;)I
+        |      13: ireturn
+        |  public java.lang.Object scalaz_tag();
+        |    Code:
+        |       0: aload_0
+        |       1: invokevirtual #25                 // Method r:()Leu/timepit/refined/scalaz/RefineAnyValTest;
+        |       4: invokevirtual #53                 // Method eu/timepit/refined/scalaz/RefineAnyValTest.scalaz_tag:()Ljava/lang/Object;
+        |       7: areturn
+        |  public int unrefined();
+        |    Code:
+        |       0: aload_0
+        |       1: invokevirtual #25                 // Method r:()Leu/timepit/refined/scalaz/RefineAnyValTest;
+        |       4: invokevirtual #56                 // Method eu/timepit/refined/scalaz/RefineAnyValTest.unrefined:()I
+        |       7: ireturn
         |  public eu.timepit.refined.scalaz.InferAnyValTest();
         |    Code:
         |       0: aload_0
-        |       1: invokespecial #55                 // Method java/lang/Object."<init>":()V
-        |       4: return
+        |       1: invokespecial #60                 // Method java/lang/Object."<init>":()V
+        |       4: aload_0
+        |       5: new           #27                 // class eu/timepit/refined/scalaz/RefineAnyValTest
+        |       8: dup
+        |       9: invokespecial #61                 // Method eu/timepit/refined/scalaz/RefineAnyValTest."<init>":()V
+        |      12: putfield      #13                 // Field r:Leu/timepit/refined/scalaz/RefineAnyValTest;
+        |      15: return
         |}
       """.stripMargin.trim
     actual ?= expected
@@ -118,53 +97,48 @@ class InferJavapSpec extends Properties("InferJavapTest") {
     val expected =
       """Compiled from "InferJavapSpec.scala"
         |public class eu.timepit.refined.scalaz.InferAnyRefTest {
-        |  public java.lang.String Refined1();
+        |  public eu.timepit.refined.scalaz.RefineAnyRefTest r();
         |    Code:
-        |       0: getstatic     #16                 // Field eu/timepit/refined/api/Refined$.MODULE$:Leu/timepit/refined/api/Refined$;
-        |       3: ldc           #18                 // String abc
-        |       5: invokevirtual #22                 // Method eu/timepit/refined/api/Refined$.unsafeApply:(Ljava/lang/Object;)Ljava/lang/Object;
-        |       8: checkcast     #24                 // class java/lang/String
-        |      11: areturn
-        |  public java.lang.String Refined0();
+        |       0: aload_0
+        |       1: getfield      #13                 // Field r:Leu/timepit/refined/scalaz/RefineAnyRefTest;
+        |       4: areturn
+        |  public java.lang.String Refined();
         |    Code:
-        |       0: getstatic     #16                 // Field eu/timepit/refined/api/Refined$.MODULE$:Leu/timepit/refined/api/Refined$;
+        |       0: getstatic     #23                 // Field eu/timepit/refined/api/Refined$.MODULE$:Leu/timepit/refined/api/Refined$;
         |       3: aload_0
-        |       4: invokevirtual #29                 // Method Refined1:()Ljava/lang/String;
-        |       7: invokevirtual #22                 // Method eu/timepit/refined/api/Refined$.unsafeApply:(Ljava/lang/Object;)Ljava/lang/Object;
-        |      10: checkcast     #24                 // class java/lang/String
-        |      13: areturn
-        |  public java.lang.String shapeless_tag1();
-        |    Code:
-        |       0: ldc           #18                 // String abc
-        |       2: areturn
-        |  public java.lang.String shapeless_tag0();
+        |       4: invokevirtual #25                 // Method r:()Leu/timepit/refined/scalaz/RefineAnyRefTest;
+        |       7: invokevirtual #29                 // Method eu/timepit/refined/scalaz/RefineAnyRefTest.Refined:()Ljava/lang/String;
+        |      10: invokevirtual #33                 // Method eu/timepit/refined/api/Refined$.unsafeApply:(Ljava/lang/Object;)Ljava/lang/Object;
+        |      13: checkcast     #35                 // class java/lang/String
+        |      16: areturn
+        |  public java.lang.String shapeless_tag();
         |    Code:
         |       0: aload_0
-        |       1: invokevirtual #33                 // Method shapeless_tag1:()Ljava/lang/String;
-        |       4: areturn
-        |  public java.lang.Object scalaz_tag1();
-        |    Code:
-        |       0: ldc           #18                 // String abc
-        |       2: areturn
-        |  public java.lang.Object scalaz_tag0();
+        |       1: invokevirtual #25                 // Method r:()Leu/timepit/refined/scalaz/RefineAnyRefTest;
+        |       4: invokevirtual #38                 // Method eu/timepit/refined/scalaz/RefineAnyRefTest.shapeless_tag:()Ljava/lang/String;
+        |       7: areturn
+        |  public java.lang.Object scalaz_tag();
         |    Code:
         |       0: aload_0
-        |       1: invokevirtual #38                 // Method scalaz_tag1:()Ljava/lang/Object;
-        |       4: areturn
-        |  public java.lang.String unrefined1();
-        |    Code:
-        |       0: ldc           #18                 // String abc
-        |       2: areturn
-        |  public java.lang.String unrefined0();
+        |       1: invokevirtual #25                 // Method r:()Leu/timepit/refined/scalaz/RefineAnyRefTest;
+        |       4: invokevirtual #42                 // Method eu/timepit/refined/scalaz/RefineAnyRefTest.scalaz_tag:()Ljava/lang/Object;
+        |       7: areturn
+        |  public java.lang.String unrefined();
         |    Code:
         |       0: aload_0
-        |       1: invokevirtual #42                 // Method unrefined1:()Ljava/lang/String;
-        |       4: areturn
+        |       1: invokevirtual #25                 // Method r:()Leu/timepit/refined/scalaz/RefineAnyRefTest;
+        |       4: invokevirtual #45                 // Method eu/timepit/refined/scalaz/RefineAnyRefTest.unrefined:()Ljava/lang/String;
+        |       7: areturn
         |  public eu.timepit.refined.scalaz.InferAnyRefTest();
         |    Code:
         |       0: aload_0
-        |       1: invokespecial #46                 // Method java/lang/Object."<init>":()V
-        |       4: return
+        |       1: invokespecial #49                 // Method java/lang/Object."<init>":()V
+        |       4: aload_0
+        |       5: new           #27                 // class eu/timepit/refined/scalaz/RefineAnyRefTest
+        |       8: dup
+        |       9: invokespecial #50                 // Method eu/timepit/refined/scalaz/RefineAnyRefTest."<init>":()V
+        |      12: putfield      #13                 // Field r:Leu/timepit/refined/scalaz/RefineAnyRefTest;
+        |      15: return
         |}
       """.stripMargin.trim
     actual ?= expected

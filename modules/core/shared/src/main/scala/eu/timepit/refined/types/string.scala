77de4b9e631ae4bb8e981ae0d5d6a3bb8e46e3ef
Frank S. Thomas <frank@timepit.eu>
2017-04-11 22:51:13 +0200

    Add TrimmedString
    A `String` that contains no leading or trailing whitespace.

--- modules/core/shared/src/main/scala/eu/timepit/refined/types/string.scala
+++ modules/core/shared/src/main/scala/eu/timepit/refined/types/string.scala
@@ -1,7 +1,9 @@
 package eu.timepit.refined.types
 
+import eu.timepit.refined.W
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.collection.NonEmpty
+import eu.timepit.refined.string.MatchesRegex
 
 /** Module for `String` refined types. */
 object string extends StringTypes
@@ -10,4 +12,7 @@ trait StringTypes {
 
   /** A `String` that is not empty. */
   type NonEmptyString = String Refined NonEmpty
+
+  /** A `String` that contains no leading or trailing whitespace. */
+  type TrimmedString = String Refined MatchesRegex[W.`"""^(?!\\s).*(?<!\\s)"""`.T]
 }

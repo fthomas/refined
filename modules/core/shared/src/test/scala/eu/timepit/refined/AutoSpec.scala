13df6194a6b5fa889682ac5c4a773849a35d74d1
Frank S. Thomas <frank@timepit.eu>
2017-05-04 23:05:51 +0200

    Try to reproduce #260

--- modules/core/shared/src/test/scala/eu/timepit/refined/AutoSpec.scala
+++ modules/core/shared/src/test/scala/eu/timepit/refined/AutoSpec.scala
@@ -4,6 +4,7 @@ import eu.timepit.refined.api.Refined
 import eu.timepit.refined.auto._
 import eu.timepit.refined.char.{Digit, Letter}
 import eu.timepit.refined.generic.Equal
+import eu.timepit.refined.types.numeric.PosInt
 import org.scalacheck.Prop._
 import org.scalacheck.Properties
 import shapeless.tag.@@
@@ -37,5 +38,10 @@ class AutoSpec extends Properties("auto") {
     val a: Char @@ Equal[W.`'0'`.T] = '0'
     illTyped("val b: Char @@ Equal[W.`'0'`.T] = '1'", """Predicate failed: \(1 == 0\).""")
     a == '0'
+  }
+
+  property("#260") = secure {
+    val somePosInt: Option[PosInt] = Some(5)
+    somePosInt.isDefined
   }
 }

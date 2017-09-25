2d461f4a1fdce943481130cb5030887cb6aaecfd
Frank S. Thomas <frank@timepit.eu>
2017-09-25 06:36:47 +0200

    Change benchmark mode to "average"

--- modules/benchmark/src/main/scala/eu/timepit/refined/benchmark/RefineMacroBenchmark.scala
+++ modules/benchmark/src/main/scala/eu/timepit/refined/benchmark/RefineMacroBenchmark.scala
@@ -1,9 +1,10 @@
 package eu.timepit.refined.benchmark
 
-import org.openjdk.jmh.annotations.{Benchmark, Scope, State}
+import org.openjdk.jmh.annotations._
 import scala.reflect.runtime.currentMirror
 import scala.tools.reflect.ToolBox
 
+@BenchmarkMode(Array(Mode.AverageTime))
 @State(Scope.Thread)
 class RefineMacroBenchmark {
   private val toolBox =

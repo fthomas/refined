2fcc42ee2a73ff8f093aa61c4e58af3ff7db9c13
Frank S. Thomas <frank@timepit.eu>
2017-10-01 20:35:33 +0200

    Optimize Validate.fromPartial
    Benchmark results for this change:
    ```
    master:
    [info] Result "eu.timepit.refined.benchmark.RefineVBenchmark.refineV_Regex":
    [info]   121.400 ±(99.9%) 1.297 ns/op [Average]
    [info]   (min, avg, max) = (110.969, 121.400, 159.030), stdev = 5.493
    [info]   CI (99.9%): [120.103, 122.698] (assumes normal distribution)
    [info] # Run complete. Total time: 00:06:43
    [info] Benchmark                       Mode  Cnt    Score   Error  Units
    [info] RefineVBenchmark.refineV_Regex  avgt  200  121.400 ± 1.297  ns/op
    
    this branch:
    [info] Result "eu.timepit.refined.benchmark.RefineVBenchmark.refineV_Regex":
    [info]   108.263 ±(99.9%) 0.893 ns/op [Average]
    [info]   (min, avg, max) = (100.935, 108.263, 138.393), stdev = 3.780
    [info]   CI (99.9%): [107.370, 109.156] (assumes normal distribution)
    [info] # Run complete. Total time: 00:06:43
    [info] Benchmark                       Mode  Cnt    Score   Error  Units
    [info] RefineVBenchmark.refineV_Regex  avgt  200  108.263 ± 0.893  ns/op
    ```

--- modules/benchmark/src/main/scala/eu/timepit/refined/benchmark/RefineVBenchmark.scala
+++ modules/benchmark/src/main/scala/eu/timepit/refined/benchmark/RefineVBenchmark.scala
@@ -1,8 +1,9 @@
 package eu.timepit.refined.benchmark
 
+import eu.timepit.refined.api.Refined
 import eu.timepit.refined.numeric.Positive
 import eu.timepit.refined.refineV
-import eu.timepit.refined.types.numeric.PosInt
+import eu.timepit.refined.string.Regex
 import java.util.concurrent.TimeUnit
 import org.openjdk.jmh.annotations.{Benchmark, BenchmarkMode, Mode, OutputTimeUnit}
 
@@ -11,6 +12,11 @@ class RefineVBenchmark {
 
   @Benchmark
   @OutputTimeUnit(TimeUnit.NANOSECONDS)
-  def refineV_Positive: Either[String, PosInt] =
+  def refineV_Positive: Either[String, Int Refined Positive] =
     refineV[Positive](1)
+
+  @Benchmark
+  @OutputTimeUnit(TimeUnit.NANOSECONDS)
+  def refineV_Regex: Either[String, String Refined Regex] =
+    refineV[Regex](".*")
 }

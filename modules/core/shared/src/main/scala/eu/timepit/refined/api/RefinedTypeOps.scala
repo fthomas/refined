adfa917a1a76ff13b84ad7208469bf378e9f8c69
Frank S. Thomas <frank@timepit.eu>
2017-12-16 16:00:35 +0100

    Add documentation to RefinedTypeOps

--- modules/core/shared/src/main/scala/eu/timepit/refined/api/RefinedTypeOps.scala
+++ modules/core/shared/src/main/scala/eu/timepit/refined/api/RefinedTypeOps.scala
@@ -1,6 +1,26 @@
 package eu.timepit.refined
 package api
 
+/**
+ * Provides functions to create values of the refined type `FTP` from
+ * values of the base type `T`. The intended usage of this class is to
+ * simplify the definition of a refined type's companion object.
+ *
+ * Example: {{{
+ * scala> import eu.timepit.refined.api.{ Refined, RefinedTypeOps }
+ *      | import eu.timepit.refined.numeric.Positive
+ *
+ * scala> type PosInt = Int Refined Positive
+ *
+ * scala> object PosInt extends RefinedTypeOps[PosInt, Int]
+ *
+ * scala> PosInt(1)
+ * res0: PosInt = 1
+ *
+ * scala> PosInt.from(2)
+ * res1: Either[String, PosInt] = Right(2)
+ * }}}
+ */
 class RefinedTypeOps[FTP, T](implicit rt: RefinedType.AuxT[FTP, T]) {
 
   def apply[F[_, _], P](t: T)(

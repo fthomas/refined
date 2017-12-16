f67e1c51d8a44e45a8644c7e0d1833d8bc9435c5
Frank S. Thomas <frank@timepit.eu>
2017-12-16 14:45:33 +0100

    Test PosInt companion

---
+++ modules/core/shared/src/test/scala/eu/timepit/refined/types/NumericTypesSpec.scala
@@ -0,0 +1,24 @@
+package eu.timepit.refined.types
+
+import eu.timepit.refined.types.numeric.PosInt
+import org.scalacheck.Prop._
+import org.scalacheck.Properties
+
+class NumericTypesSpec extends Properties("NumericTypes") {
+
+  property("PosInt.from(1)") = secure {
+    PosInt.from(1) ?= Right(PosInt(1))
+  }
+
+  property("PosInt.from(-1)") = secure {
+    PosInt.from(-1) ?= Left("Predicate failed: (-1 > 0).")
+  }
+
+  property("PosInt.unsafeFrom(1)") = secure {
+    PosInt.unsafeFrom(1) ?= PosInt(1)
+  }
+
+  property("PosInt.unsafeFrom(-1)") = secure {
+    throws(classOf[IllegalArgumentException])(PosInt.unsafeFrom(-1))
+  }
+}

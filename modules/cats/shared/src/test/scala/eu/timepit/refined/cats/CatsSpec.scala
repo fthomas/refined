efd57b539b8e8ca07529a220411a937071945ac4
Frank S. Thomas <frank@timepit.eu>
2017-12-18 21:34:33 +0100

    Do not use auto macros in CatsSpec

--- modules/cats/shared/src/test/scala/eu/timepit/refined/cats/CatsSpec.scala
+++ modules/cats/shared/src/test/scala/eu/timepit/refined/cats/CatsSpec.scala
@@ -3,7 +3,6 @@ package eu.timepit.refined.cats
 import cats.instances.int._
 import cats.syntax.eq._
 import cats.syntax.show._
-import eu.timepit.refined.auto._
 import eu.timepit.refined.types.numeric.PosInt
 import org.scalacheck.Prop._
 import org.scalacheck.Properties
@@ -11,13 +10,10 @@ import org.scalacheck.Properties
 class CatsSpec extends Properties("cats") {
 
   property("Equal") = secure {
-    val x: PosInt = 5
-    val y: PosInt = 5
-    x === y
+    PosInt.unsafeFrom(5) === PosInt.unsafeFrom(5)
   }
 
   property("Show") = secure {
-    val x: PosInt = 5
-    x.show ?= "5"
+    PosInt.unsafeFrom(5).show ?= "5"
   }
 }

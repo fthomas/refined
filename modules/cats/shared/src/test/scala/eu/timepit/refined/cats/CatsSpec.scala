f57b3e01bdc9de7324706d19993657b00bdf3081
Howy Perrin <howyp@users.noreply.github.com>
2017-12-20 18:38:33 +0000

    Add .validate method for refined companions

--- modules/cats/shared/src/test/scala/eu/timepit/refined/cats/CatsSpec.scala
+++ modules/cats/shared/src/test/scala/eu/timepit/refined/cats/CatsSpec.scala
@@ -1,5 +1,6 @@
 package eu.timepit.refined.cats
 
+import cats.data.{NonEmptyList, Validated}
 import cats.implicits._
 import eu.timepit.refined.types.numeric.PosInt
 import org.scalacheck.Prop._
@@ -11,16 +12,26 @@ class CatsSpec extends Properties("cats") {
     val refTypeOrder: Unit = () // shadow the `Order` instance so the `Eq` instance is tested
     locally(refTypeOrder) // prevent a "unused" warning
 
-    PosInt.unsafeFrom(5) === PosInt.unsafeFrom(5)
+    PosInt(5) === PosInt(5)
   }
 
   property("Order") = secure {
-    val x = PosInt.unsafeFrom(5)
-    val y = PosInt.unsafeFrom(6)
+    val x = PosInt(5)
+    val y = PosInt(6)
     x min y ?= x
   }
 
   property("Show") = secure {
-    PosInt.unsafeFrom(5).show ?= "5"
+    PosInt(5).show ?= "5"
+  }
+
+  property("Validate when Valid") = secure {
+    import validation._
+    PosInt.validate(5) ?= Validated.Valid(PosInt(5))
+  }
+
+  property("Validate when Invalid") = secure {
+    import validation._
+    PosInt.validate(0) ?= Validated.Invalid(NonEmptyList("Predicate failed: (0 > 0).", Nil))
   }
 }

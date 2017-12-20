562b7180d5a521d6d6922d8e21762eb8bd811ad1
Howy Perrin <howyp@users.noreply.github.com>
2017-12-20 21:57:29 +0000

    Use cat's .toValidatedNel in CatsValidated

--- modules/cats/shared/src/main/scala/eu/timepit/refined/cats/CatsValidated.scala
+++ modules/cats/shared/src/main/scala/eu/timepit/refined/cats/CatsValidated.scala
@@ -1,11 +1,11 @@
 package eu.timepit.refined.cats
 
-import cats.data.{NonEmptyList, Validated, ValidatedNel}
+import cats.data.ValidatedNel
+import cats.syntax.either._
 import eu.timepit.refined.api.RefinedTypeOps
 
 object validation {
   implicit class CatsValidateOps[FTP, T](companion: RefinedTypeOps[FTP, T]) {
-    def validate(value: T): ValidatedNel[String, FTP] =
-      Validated.fromEither(companion.from(value)).leftMap(NonEmptyList.one)
+    def validate(value: T): ValidatedNel[String, FTP] = companion.from(value).toValidatedNel
   }
 }

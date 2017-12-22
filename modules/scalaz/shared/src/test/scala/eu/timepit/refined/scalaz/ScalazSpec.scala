e225bca3d2e4806e79baf96fee616aa466d2df22
Jose Fernando Go-Oco <882152+kusamakura@users.noreply.github.com>
2017-12-22 13:13:49 +0900

    Add scalaz syntax object (as in #384)

--- modules/scalaz/shared/src/test/scala/eu/timepit/refined/scalaz/ScalazSpec.scala
+++ modules/scalaz/shared/src/test/scala/eu/timepit/refined/scalaz/ScalazSpec.scala
@@ -1,6 +1,8 @@
 package eu.timepit.refined.scalaz
 
-import eu.timepit.refined.numeric.Positive
+import eu.timepit.refined.W
+import eu.timepit.refined.api.{Refined, RefinedTypeOps}
+import eu.timepit.refined.numeric.Interval
 import eu.timepit.refined.types.numeric.PosInt
 import org.scalacheck.Prop._
 import org.scalacheck.Properties
@@ -8,18 +10,18 @@ import scalaz.Validation
 
 class ScalazSpec extends Properties("scalaz") {
   property("Validate when Valid") = secure {
-    import validation._
+    import syntax._
     PosInt.validate(5) ?= Validation.success(PosInt.unsafeFrom(5))
   }
 
   property("Validate when Invalid") = secure {
-    import validation._
+    import syntax._
     PosInt.validate(0) ?= Validation.failureNel("Predicate failed: (0 > 0).")
   }
 
-  property("Validate as syntax on the raw type") = secure {
-    import validation._
-    val x = 5
-    x.validate[Positive] ?= Validation.success(PosInt.unsafeFrom(5))
+  property("validate without import") = secure {
+    type OneToTen = Int Refined Interval.Closed[W.`1`.T, W.`10`.T]
+    object OneToTen extends RefinedTypeOps[OneToTen, Int] with ScalazRefinedTypeOpsSyntax
+    OneToTen.validate(5) ?= Validation.success(OneToTen.unsafeFrom(5))
   }
 }

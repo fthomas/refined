c4a8f8445a823eaa9fd230beb98279e3137be845
Howy Perrin <howyp@users.noreply.github.com>
2017-12-24 19:55:14 +0000

    Add `Arbitrary` instance based on Max/Min

--- modules/scalacheck/shared/src/main/scala/eu/timepit/refined/scalacheck/numeric.scala
+++ modules/scalacheck/shared/src/main/scala/eu/timepit/refined/scalacheck/numeric.scala
@@ -1,6 +1,6 @@
 package eu.timepit.refined.scalacheck
 
-import eu.timepit.refined.api.{RefType, Validate}
+import eu.timepit.refined.api._
 import eu.timepit.refined.numeric._
 import eu.timepit.refined.scalacheck.util.{Adjacent, Bounded}
 import org.scalacheck.{Arbitrary, Gen}
@@ -14,23 +14,7 @@ import shapeless.ops.nat.ToInt
  */
 object numeric extends NumericInstances
 
-trait NumericInstances {
-
-  /**
-   * A generator that generates a random value in the given (inclusive)
-   * range that satisfies the predicate `P`. If the range is invalid,
-   * the generator will not generate any value.
-   *
-   * This is like ScalaCheck's `Gen.chooseNum` but for refined types.
-   */
-  def chooseRefinedNum[F[_, _], T: Numeric: Choose, P](min: F[T, P], max: F[T, P])(
-      implicit rt: RefType[F],
-      v: Validate[T, P]
-  ): Gen[F[T, P]] =
-    Gen.chooseNum(rt.unwrap(min), rt.unwrap(max)).filter(v.isValid).map(rt.unsafeWrap)
-
-  ///
-
+trait DeprecatedNumericInstances {
   implicit def lessArbitraryWit[F[_, _]: RefType, T: Numeric: Choose: Adjacent, N <: T](
       implicit bounded: Bounded[T],
       wn: Witness.Aux[N]
@@ -140,4 +124,28 @@ trait NumericInstances {
       min: T,
       max: T): Arbitrary[F[T, P]] =
     arbitraryRefType(Gen.chooseNum(min, max))
+
+}
+
+trait NumericInstances extends DeprecatedNumericInstances {
+
+  /**
+   * A generator that generates a random value in the given (inclusive)
+   * range that satisfies the predicate `P`. If the range is invalid,
+   * the generator will not generate any value.
+   *
+   * This is like ScalaCheck's `Gen.chooseNum` but for refined types.
+   */
+  def chooseRefinedNum[F[_, _], T: Numeric: Choose, P](min: F[T, P], max: F[T, P])(
+      implicit rt: RefType[F],
+      v: Validate[T, P]
+  ): Gen[F[T, P]] =
+    Gen.chooseNum(rt.unwrap(min), rt.unwrap(max)).filter(v.isValid).map(rt.unsafeWrap)
+
+  ///
+
+  implicit def arbitraryFromMaxAndMin[C, N](implicit min: Min[C Refined N],
+                                            max: Max[C Refined N],
+                                            choose: Choose[C]): Arbitrary[C Refined N] =
+    Arbitrary(Gen.choose(min.min.value, max.max.value).map(Refined.unsafeApply[C, N]))
 }

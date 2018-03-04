e18ad13387900963e1a48bbb95ca1f30ba07225f
Frank S. Thomas <frank@timepit.eu>
2018-03-04 22:00:50 +0100

    Port of #447 for Cats

--- modules/cats/shared/src/main/scala/eu/timepit/refined/cats/package.scala
+++ modules/cats/shared/src/main/scala/eu/timepit/refined/cats/package.scala
@@ -1,9 +1,11 @@
 package eu.timepit.refined
 
+import _root_.cats.Contravariant
+import _root_.cats.MonadError
 import _root_.cats.Show
 import _root_.cats.implicits._
 import _root_.cats.kernel.{Eq, Order}
-import eu.timepit.refined.api.RefType
+import eu.timepit.refined.api.{RefType, Validate}
 
 package object cats {
 
@@ -27,4 +29,18 @@ package object cats {
    */
   implicit def refTypeShow[F[_, _], T: Show, P](implicit rt: RefType[F]): Show[F[T, P]] =
     Show[T].contramap(rt.unwrap)
+
+  implicit def refTypeContravariant[F[_, _], G[_], T, P](
+      implicit c: Contravariant[G],
+      rt: RefType[F],
+      gt: G[T]
+  ): G[F[T, P]] = c.contramap(gt)(rt.unwrap)
+
+  implicit def refTypeMonadError[F[_, _], G[_], T, P](
+      implicit m: MonadError[G, String],
+      rt: RefType[F],
+      v: Validate[T, P],
+      gt: G[T]
+  ): G[F[T, P]] =
+    m.flatMap(gt)(t => m.fromEither(rt.refine[P](t)))
 }

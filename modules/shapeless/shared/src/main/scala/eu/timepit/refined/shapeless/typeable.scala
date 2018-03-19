d825a2ee6e04c4d316ad63e734f3d5e91e0def74
Didac Montero <didac.monteromendez@epfl.ch>
2018-03-19 09:55:57 +0100

    Changed toOption to match in order to support previous scala versions

--- modules/shapeless/shared/src/main/scala/eu/timepit/refined/shapeless/typeable.scala
+++ modules/shapeless/shared/src/main/scala/eu/timepit/refined/shapeless/typeable.scala
@@ -2,6 +2,7 @@ package eu.timepit.refined.shapeless
 
 import _root_.shapeless.Typeable
 import eu.timepit.refined.api.{RefType, Validate}
+import scala.util.Right
 
 package object typeable {
 
@@ -14,7 +15,12 @@ package object typeable {
                                               P: Typeable[P]): Typeable[F[T, P]] =
     new Typeable[F[T, P]] {
       override def cast(t: Any): Option[F[T, P]] =
-        T.cast(t).flatMap(v => rt.refine[P](v).toOption)
+        T.cast(t)
+          .flatMap(casted =>
+            rt.refine[P](casted) match {
+              case Right(v) => Some(v)
+              case _        => None
+          })
       override def describe: String = s"Refined[${T.describe}, ${P.describe}]"
     }
 }

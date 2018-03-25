ae4274947d7d539ca0d06f8412a341daba10f303
Matt Pickering <matthedude@hotmail.com>
2018-03-25 15:51:29 +0100

    Remove Nat predicate restriction and incorrect upper bound. Test predicates guaranteed to not produce dangerous results.

--- modules/scalacheck/shared/src/test/scala/eu/timepit/refined/scalacheck/CollectionArbitrarySpec.scala
+++ modules/scalacheck/shared/src/test/scala/eu/timepit/refined/scalacheck/CollectionArbitrarySpec.scala
@@ -1,7 +1,9 @@
 package eu.timepit.refined.scalacheck
 
+import eu.timepit.refined.W
 import eu.timepit.refined.api.Refined
-import eu.timepit.refined.collection.{MaxSize, MinSize}
+import eu.timepit.refined.collection.{MaxSize, Size}
+import eu.timepit.refined.numeric.Interval
 import eu.timepit.refined.scalacheck.collection._
 import eu.timepit.refined.scalacheck.numeric._
 import org.scalacheck.Properties
@@ -9,7 +11,10 @@ import shapeless.nat._
 
 class CollectionArbitrarySpec extends Properties("CollectionArbitrary") {
 
-  property("MaxSize") = checkArbitraryRefType[Refined, List[String], MaxSize[_10]]
+  property("MaxSize.positive") = checkArbitraryRefType[Refined, List[String], MaxSize[W.`42`.T]]
 
-  property("MinSize") = checkArbitraryRefType[Refined, List[String], MinSize[_10]]
+  property("MaxSize.Nat") = checkArbitraryRefType[Refined, List[String], MaxSize[_13]]
+
+  property("Size.Interval.Closed") =
+    checkArbitraryRefType[Refined, List[String], Size[Interval.Closed[W.`23`.T, W.`42`.T]]]
 }

dbd4dbc971fcb972ed721501055ba8eca6620c72
Frank S. Thomas <f.thomas@gmx.de>
2018-05-26 15:39:30 +0200

    Improve test coverage of RefinedTypeOps (#521)
    * Improve test coverage of RefinedTypeOps
    
    * Fix compile errors with Scala < 2.12

---
+++ modules/core/shared/src/test/scala/eu/timepit/refined/api/RefinedTypeOpsSpec.scala
@@ -0,0 +1,21 @@
+package eu.timepit.refined.api
+
+import eu.timepit.refined.types.numeric.NonNegInt
+import org.scalacheck.Prop._
+import org.scalacheck.Properties
+import scala.util.{Failure, Success, Try}
+
+class RefinedTypeOpsSpec extends Properties("RefinedTypeOps") {
+
+  property("from ~= unapply") = forAll { i: Int =>
+    NonNegInt.from(i).right.toOption ?= NonNegInt.unapply(i)
+  }
+
+  property("from ~= unsafeFrom") = forAll { i: Int =>
+    val stringOrNonNegInt = Try(NonNegInt.unsafeFrom(i)) match {
+      case Success(n) => Right(n)
+      case Failure(t) => Left(t.getMessage)
+    }
+    NonNegInt.from(i) ?= stringOrNonNegInt
+  }
+}
--- project/plugin-scalafmt.sbt
+++ project/plugin-scalafmt.sbt
@@ -1,1 +1,1 @@
-addSbtPlugin("com.geirsson" % "sbt-scalafmt" % "1.4.0")
+addSbtPlugin("com.geirsson" % "sbt-scalafmt" % "1.5.1")

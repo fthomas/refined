1ab598329bdae9bd6df1c2d7874de97b87523e12
Chris Birchall <chris.birchall@gmail.com>
2018-07-06 23:57:15 +0100

    Add syntax for Cats NonEmptyList

--- modules/cats/shared/src/main/scala/eu/timepit/refined/cats/syntax.scala
+++ modules/cats/shared/src/main/scala/eu/timepit/refined/cats/syntax.scala
@@ -1,14 +1,21 @@
 package eu.timepit.refined.cats
 
-import cats.data.ValidatedNel
+import cats.data.{NonEmptyList, ValidatedNel}
 import cats.syntax.either._
 import eu.timepit.refined.api.RefinedTypeOps
+import eu.timepit.refined.types.numeric.PosInt
 
-object syntax extends CatsRefinedTypeOpsSyntax
+object syntax extends CatsRefinedTypeOpsSyntax with CatsNonEmptyListSyntax
 
 trait CatsRefinedTypeOpsSyntax {
   implicit class CatsRefinedTypeOps[FTP, T](rtOps: RefinedTypeOps[FTP, T]) {
     def validate(t: T): ValidatedNel[String, FTP] =
       rtOps.from(t).toValidatedNel
+  }
+}
+
+trait CatsNonEmptyListSyntax {
+  implicit class CatsNonEmptyListRefinedOps[A](nel: NonEmptyList[A]) {
+    def refinedSize: PosInt = PosInt.unsafeFrom(nel.size)
   }
 }

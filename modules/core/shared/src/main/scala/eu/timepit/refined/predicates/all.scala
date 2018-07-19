3db47526caddf2a07f314cb70a0a62fd57e0ddf8
Eric Honer <eric.honer@tstllc.net>
2018-07-18 22:36:34 -0400

    fix binary compatibility

--- modules/core/shared/src/main/scala/eu/timepit/refined/predicates/all.scala
+++ modules/core/shared/src/main/scala/eu/timepit/refined/predicates/all.scala
@@ -1,14 +1,15 @@
 package eu.timepit.refined.predicates
 
-object all extends AllPredicates with AllPredicatesBinCompat1
+object all extends AllPredicates with AllPredicatesBinCompat1 with AllPredicatesBinCompat2
 
 trait AllPredicates
     extends BooleanPredicates
     with CharPredicates
     with CollectionPredicates
     with GenericPredicates
-    with ImpurePredicates
     with NumericPredicates
     with StringPredicates
 
 trait AllPredicatesBinCompat1 extends StringPredicatesBinCompat1
+
+trait AllPredicatesBinCompat2 extends ImpurePredicates

b6a4f0a916612b371e3892b8dc3537caf30f6c06
Eric Honer <eric.honer@tstllc.net>
2018-07-18 21:38:17 -0400

    scalafmt fixes

--- modules/core/shared/src/main/scala/eu/timepit/refined/impure.scala
+++ modules/core/shared/src/main/scala/eu/timepit/refined/impure.scala
@@ -17,19 +17,18 @@ object impure {
       new Validate[T, NonNull[A]] {
         override type R = NonNull[Either[Null, v.Res]]
 
-        override def validate(t: T): Res = {
+        override def validate(t: T): Res =
           if (t == null) Failed(NonNull(Left(Null())))
           else {
             val ra = v.validate(t)
             Result.fromBoolean(ra.isPassed, NonNull(Right(ra)))
           }
-        }
 
-        override def showExpr(t: T): String = {
+        override def showExpr(t: T): String =
           if (t == null) "source is null"
-          else if (v.validate(t).isFailed) Resources.showResultAndLeftFailed("NonNull passed", v.showExpr(t))
+          else if (v.validate(t).isFailed)
+            Resources.showResultAndLeftFailed("NonNull passed", v.showExpr(t))
           else Resources.showResultAndBothPassed(s"NonNull and ${v.showExpr(t)}")
-        }
       }
   }
 }

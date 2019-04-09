92d2ab6d1e295d6caee868e73d99e3de72a0e97a
dm-tran <dm-tran@users.noreply.github.com>
2019-04-09 13:50:20 +0200

    use MinValue and MaxValue in SemigroupSpec

--- modules/cats/shared/src/test/scala/eu/timepit/refined/cats/SemigroupSpec.scala
+++ modules/cats/shared/src/test/scala/eu/timepit/refined/cats/SemigroupSpec.scala
@@ -8,21 +8,21 @@ import org.scalacheck.Properties
 class SemigroupSpec extends Properties("Semigroup") {
 
   property("Positive Integral types overflow") =
-    secure { (PosByte.MaxValue |+| PosByte.unsafeFrom(1)) === PosByte.unsafeFrom(1) } &&
-      secure { (PosShort.MaxValue |+| PosShort.unsafeFrom(1)) === PosShort.unsafeFrom(1) } &&
-      secure { (PosInt.MaxValue |+| PosInt.unsafeFrom(1)) === PosInt.unsafeFrom(1) } &&
-      secure { (PosLong.MaxValue |+| PosLong.unsafeFrom(1)) === PosLong.unsafeFrom(1) }
+    secure { (PosByte.MaxValue |+| PosByte.unsafeFrom(1)) === PosByte.MinValue } &&
+      secure { (PosShort.MaxValue |+| PosShort.unsafeFrom(1)) === PosShort.MinValue } &&
+      secure { (PosInt.MaxValue |+| PosInt.unsafeFrom(1)) === PosInt.MinValue } &&
+      secure { (PosLong.MaxValue |+| PosLong.unsafeFrom(1)) === PosLong.MinValue }
 
   property("NonNegative Integral types overflow") =
-    secure { (NonNegByte.MaxValue |+| NonNegByte.unsafeFrom(1)) === NonNegByte.unsafeFrom(0) } &&
-      secure { (NonNegShort.MaxValue |+| NonNegShort.unsafeFrom(1)) === NonNegShort.unsafeFrom(0) } &&
-      secure { (NonNegInt.MaxValue |+| NonNegInt.unsafeFrom(1)) === NonNegInt.unsafeFrom(0) } &&
-      secure { (NonNegLong.MaxValue |+| NonNegLong.unsafeFrom(1)) === NonNegLong.unsafeFrom(0) }
+    secure { (NonNegByte.MaxValue |+| NonNegByte.unsafeFrom(1)) === NonNegByte.MinValue } &&
+      secure { (NonNegShort.MaxValue |+| NonNegShort.unsafeFrom(1)) === NonNegShort.MinValue } &&
+      secure { (NonNegInt.MaxValue |+| NonNegInt.unsafeFrom(1)) === NonNegInt.MinValue } &&
+      secure { (NonNegLong.MaxValue |+| NonNegLong.unsafeFrom(1)) === NonNegLong.MinValue }
 
   property("Negative Integral types overflow") =
-    secure { (NegByte.MinValue |+| NegByte.unsafeFrom(-1)) === NegByte.unsafeFrom(-1) } &&
-      secure { (NegShort.MinValue |+| NegShort.unsafeFrom(-1)) === NegShort.unsafeFrom(-1) } &&
-      secure { (NegInt.MinValue |+| NegInt.unsafeFrom(-1)) === NegInt.unsafeFrom(-1) } &&
-      secure { (NegLong.MinValue |+| NegLong.unsafeFrom(-1)) === NegLong.unsafeFrom(-1) }
+    secure { (NegByte.MinValue |+| NegByte.unsafeFrom(-1)) === NegByte.MaxValue } &&
+      secure { (NegShort.MinValue |+| NegShort.unsafeFrom(-1)) === NegShort.MaxValue } &&
+      secure { (NegInt.MinValue |+| NegInt.unsafeFrom(-1)) === NegInt.MaxValue } &&
+      secure { (NegLong.MinValue |+| NegLong.unsafeFrom(-1)) === NegLong.MaxValue }
 
 }

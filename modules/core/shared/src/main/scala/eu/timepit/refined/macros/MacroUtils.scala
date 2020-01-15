0ba69efee63dc50413dae16901716039d46c9699
Frank S. Thomas <frank@timepit.eu>
2020-01-15 14:31:05 +0100

    Do not wrap c.eval in Try

--- modules/core/shared/src/main/scala/eu/timepit/refined/macros/MacroUtils.scala
+++ modules/core/shared/src/main/scala/eu/timepit/refined/macros/MacroUtils.scala
@@ -2,7 +2,6 @@ package eu.timepit.refined.macros
 
 import eu.timepit.refined.api.{Refined, RefType}
 import scala.reflect.macros.blackbox
-import scala.util.{Success, Try}
 import shapeless.tag.@@
 
 trait MacroUtils {
@@ -16,14 +15,8 @@ trait MacroUtils {
     // Duplicate and untypecheck before calling `eval`, see:
     // http://www.scala-lang.org/api/2.12.0/scala-reflect/scala/reflect/macros/Evals.html#eval[T]%28expr:Evals.this.Expr[T]%29:T
     val expr = c.Expr[T](c.untypecheck(t.tree.duplicate))
-
-    // Try evaluating expr twice before failing, see
-    // https://github.com/fthomas/refined/issues/3
-    tryN(2, c.eval(expr))
+    c.eval(expr)
   }
-
-  def tryN[T](n: Int, t: => T): T =
-    Stream.fill(n)(Try(t)).collectFirst { case Success(r) => r }.getOrElse(t)
 
   protected def refTypeInstance[F[_, _]](rt: c.Expr[RefType[F]]): RefType[F] =
     if (rt.tree.tpe =:= weakTypeOf[RefType[Refined]])

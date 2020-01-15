2d74d64398281ad8c5525917a70ca7ac41b3c5c4
Frank S. Thomas <frank@timepit.eu>
2020-01-15 15:05:17 +0100

    tryN is still needed

--- modules/core/shared/src/main/scala/eu/timepit/refined/macros/MacroUtils.scala
+++ modules/core/shared/src/main/scala/eu/timepit/refined/macros/MacroUtils.scala
@@ -16,7 +16,10 @@ trait MacroUtils {
     // Duplicate and untypecheck before calling `eval`, see:
     // http://www.scala-lang.org/api/2.12.0/scala-reflect/scala/reflect/macros/Evals.html#eval[T]%28expr:Evals.this.Expr[T]%29:T
     val expr = c.Expr[T](c.untypecheck(t.tree.duplicate))
-    c.eval(expr)
+
+    // Try evaluating expr twice before failing, see
+    // https://github.com/fthomas/refined/issues/3
+    tryN(2, c.eval(expr))
   }
 
   def tryN[T](n: Int, t: => T): T =

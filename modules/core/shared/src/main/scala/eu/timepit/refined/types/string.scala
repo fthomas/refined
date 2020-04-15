f81dc4274e4c60a688b53bf80611ba4f07205ab0
Jente Hidskes <jente.tibor-hidskes@klarna.com>
2020-04-15 18:55:16 +0200

    Add new NonEmptyFiniteString type
    This type is a conjunction of the existing FiniteString and NonEmptyString
    types.

--- modules/core/shared/src/main/scala/eu/timepit/refined/types/string.scala
+++ modules/core/shared/src/main/scala/eu/timepit/refined/types/string.scala
@@ -1,8 +1,10 @@
 package eu.timepit.refined.types
 
 import eu.timepit.refined.api.{Refined, RefinedType, RefinedTypeOps}
-import eu.timepit.refined.collection.{MaxSize, NonEmpty}
+import eu.timepit.refined.collection.{MaxSize, NonEmpty, Size}
+import eu.timepit.refined.numeric.Interval
 import eu.timepit.refined.string.{HexStringSpec, Trimmed}
+import shapeless.Nat._1
 import shapeless.Witness
 
 /** Module for `String` refined types. */
@@ -61,6 +63,39 @@ object string {
 
   object NonEmptyString extends RefinedTypeOps[NonEmptyString, String]
 
+  /** A `String` that is not empty with length less than or equal to `N`. */
+  type NonEmptyFiniteString[N] = String Refined Size[Interval.Closed[_1, N]]
+
+  object NonEmptyFiniteString {
+    class NonEmptyFiniteStringOps[N <: Int](
+        implicit
+        rt: RefinedType.AuxT[NonEmptyFiniteString[N], String],
+        wn: Witness.Aux[N]
+    ) extends RefinedTypeOps[NonEmptyFiniteString[N], String] {
+
+      /** The maximum length of a `NonEmptyFiniteString[N]`. */
+      final val maxLength: N = wn.value
+    }
+
+    /**
+     * Creates a "companion object" for `NonEmptyFiniteString[N]` with a fixed `N`.
+     *
+     * Example: {{{
+     * scala> import eu.timepit.refined.W
+     *      | import eu.timepit.refined.types.string.NonEmptyFiniteString
+     *
+     * scala> val NEFString4 = NonEmptyFiniteString[W.`4`.T]
+     * scala> NEFString4.from("abcd")
+     * res1: Either[String, NonEmptyFiniteString[W.`4`.T]] = Right(abcd)
+     * }}}
+     */
+    def apply[N <: Int](
+        implicit
+        rt: RefinedType.AuxT[NonEmptyFiniteString[N], String],
+        wn: Witness.Aux[N]
+    ): NonEmptyFiniteStringOps[N] = new NonEmptyFiniteStringOps[N]
+  }
+
   /** A `String` that contains no leading or trailing whitespace. */
   type TrimmedString = String Refined Trimmed
 
@@ -82,6 +117,9 @@ trait StringTypes {
 
   final type NonEmptyString = string.NonEmptyString
   final val NonEmptyString = string.NonEmptyString
+
+  final type NonEmptyFiniteString[N] = string.NonEmptyFiniteString[N]
+  final val NonEmptyFiniteString = string.NonEmptyFiniteString
 
   final type TrimmedString = string.TrimmedString
   final val TrimmedString = string.TrimmedString

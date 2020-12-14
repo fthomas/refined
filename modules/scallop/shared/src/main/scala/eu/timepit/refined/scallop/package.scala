bee12fc3f7cb63caca9ad036ca58aa4eb84a9e34
Platon Pronko <platon7pronko@gmail.com>
2020-12-14 13:22:48 +0300

    fix formatting

--- modules/scallop/shared/src/main/scala/eu/timepit/refined/scallop/package.scala
+++ modules/scallop/shared/src/main/scala/eu/timepit/refined/scallop/package.scala
@@ -5,11 +5,10 @@ import eu.timepit.refined.api.{RefType, Validate}
 
 package object scallop {
 
-  implicit def refTypeConverter[F[_, _], T, P](
-    implicit
-    converter: ValueConverter[T],
-    refType: RefType[F],
-    validate: Validate[T, P]
+  implicit def refTypeConverter[F[_, _], T, P](implicit
+      converter: ValueConverter[T],
+      refType: RefType[F],
+      validate: Validate[T, P]
   ): ValueConverter[F[T, P]] = converter.map(t => refType.refine[P].unsafeFrom(t))
 
 }
